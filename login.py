38b01063ccbbbe268cf6fdc7eeff9a2a968dc0f6
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-02-02 09:12:56 +0100

    Update login.py

--- login.py
+++ login.py
@@ -6,4 +6,5 @@ l1=Label(s,background="pink",text="Username",font=("cambria",40)).place(x=50, y=
 l2=Label(s,background="pink",text="Password",font=("cambria",40)).place(x=50, y=150)
 c1=Entry(s,bg="white",fg="black",width=15,font=("cambria",40)).place(x=300, y=50)
 c2=Entry(s,bg="white",fg="black",width=15,font=("cambria",40)).place(x=300, y=150)
+b2=Button(s,width=50,height=5,text="submit",background="white",font=("cambria",15),command=s.destroy).place(x=100,y=350)
 s.mainloop()
