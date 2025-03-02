589e9132c01acbbe65b181d07e7bfe92a527e4db
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-03-02 09:57:06 +0100

    Update timestable.py

--- timestable.py
+++ timestable.py
@@ -5,13 +5,14 @@ s.config(background="black")
 s.geometry("400x800")
 s.title("Mathmetical Table")
 def command1():
-    tables=()
+    tables=""
     for i in range(r1.get()+1):
         tables+=str(d1.get())+" * "+str(i)+" = "+str(d1.get()*i)+"\n"
     l3.configure(text=tables)
 l1=Label(s,text="Number and range:",font=("arial",10),background="black",foreground="white").place(x=20,y=100)
 l2=Label(s,text="Mathmetical Table",font=("arial",10),background="black",foreground="white").place(x=160,y=20)
-l3=Label(s,font=("arial",10),background="black",foreground="white",anchor="center").place(x=160,y=20)
+l3=Label(s,font=("arial",12),background="black",foreground="white",anchor="center")
+l3.place(x=155,y=220)
 d1=IntVar()
 dropdown=Combobox(s,textvariable=d1,width=7)
 #adding values to the dropdown
