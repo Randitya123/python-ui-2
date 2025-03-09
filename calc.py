ec9970f6d4c618544f272048f5a81e8aeb10aa3d
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-03-09 09:56:47 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,10 +2,26 @@ from tkinter import*
 from tkinter.ttk import*
 
 s=Tk()
-
+expression=""
 e1var=StringVar()
 e1=Entry(s,width=20,textvariable=e1var,font=("Arial",15))
 e1.grid(row=0,column=0,columnspan=4,pady=10)
+def press(num):
+    global expression
+    expression+=num
+    e1var.set(expression)
+def clear():
+    global expression
+    e1var.set("")
+def calculatiobnioc():
+    try:
+        global expression
+        result=str(eval(expression))
+        e1var.set(result)
+        expression=""
+    except:
+        e1var.set("error")
+        expression=""
 #button positon
 l1=[
     ("7",1,0),("8",1,1),("9",1,2),("/",1,3),
@@ -16,8 +32,10 @@ l1=[
 #creating the buttons
 for (text,row,column) in l1:
     if text=="=":
-        b1=Button(s,width=5,text=text,command=s.destroy)
+        b1=Button(s,width=5,text=text,command=calculatiobnioc)
     else:
-        b1=Button(s,width=5,text=text,)
-    b1.grid(row=row,column=column,columnspan=4,pady=5)
+        b1=Button(s,width=5,text=text,command=lambda t=text:press(t))
+    b1.grid(row=row,column=column,padx=5,pady=5)
+b2=Button(s,width=5,text="C",command=clear)
+b2.grid(row=5,column=0,columnspan=4,pady=5)
 s.mainloop()
