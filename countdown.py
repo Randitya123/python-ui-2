2376828227e77b7efba14a49e4453a5092bda6fe
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-03-16 09:27:20 +0100

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,20 +1,20 @@
 from tkinter import*
 from tkinter import messagebox
 import time
-s=Tk()
-s.geometry("100x100")
+screen=Tk()
+screen.geometry("100x100")
 
 h=StringVar()
 m=StringVar()
-sec=StringVar()
+s=StringVar()
 h.set("00")
 m.set("00")
-sec.set("00")
-e1=Entry(s,width=3,textvariable=h,font=4)
+s.set("00")
+e1=Entry(screen,width=3,textvariable=h,font=4)
 e1.place(x=5,y=20)
-e2=Entry(s,width=3,textvariable=m,font=4)
+e2=Entry(screen,width=3,textvariable=m,font=4)
 e2.place(x=45,y=20)
-e3=Entry(s,width=3,textvariable=s,font=4)
+e3=Entry(screen,width=3,textvariable=s,font=4)
 e3.place(x=85,y=20)
 def times():
     try:
@@ -30,9 +30,9 @@ def times():
         h.set("{00:2d}".format(hour))
         m.set("{00:2d}".format(min))
         s.set("{00:2d}".format(secs))
-        s.update()
+        screen.update()
         time.sleep(1)
         temptime-=1
-b1=Button(s,text="set time countdown",width=15,background="gold",command=times)
+b1=Button(screen,text="set time countdown",width=15,background="gold",command=times)
 b1.place(x=2,y=60)
-s.mainloop()
+screen.mainloop()
