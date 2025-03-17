e7dcf4b90a9ffae3b11ee9af393655ac5d0c6650
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-03-17 19:20:15 +0100

    Update voice.py

--- voice.py
+++ voice.py
@@ -3,12 +3,20 @@ from gtts import gTTS
 import os
 screen=Tk()
 screen.geometry("300x360")
-f1=Frame(screen,bg="red",height="150")
+f1=Frame(screen,bg="Black",height="150")
 f1.pack(fill=X)
-f2=Frame(screen,bg="green",height="220")
+f2=Frame(screen,bg="Purple",height="220")
 f2.pack(fill=X)
-l1=Label(f1,text="Text to speech",font=("Cambria",15))
-l1.place(x=80,y=100)
+l1=Label(f1,text="Text to speech",background="black",foreground="white",font=("Cambria",15))
+l1.place(x=80,y=70)
 e1=Entry(f2,width=20,bd=3,font=10)
-e1.place(x=50,y=100)
+e1.place(x=50,y=80)
+def textvoice():
+    language="en"
+    obj1=gTTS(text=e1.get(),lang=language,slow=False)
+    obj1.save("voice.wav")
+    os.system("voice.wav")
+b1=Button(f2,width=5,text="Submit",fg="White",background="black",command=textvoice)
+b1.place(x=120,y=150)
+
 screen.mainloop()
